9ca4b6cb5a002ea0e306c25c9d87598cdc173343
Mirkopoj <mirkopoj@hotmail.com>
2022-04-16 12:45:40 -0300

    Funcion Delta

--- Interpretacion_FFT.py
+++ Interpretacion_FFT.py
@@ -10,6 +10,7 @@ M = 9
 
 #__Ejes__################################
 fstep = 1/M                             #
+R = 2*M                                 #
                                         #
 def flim(S):                            #
     return int(S/2)*fstep               #
@@ -17,7 +18,8 @@ def tlim(S):                            #
     return int(S/2)                     #
                                         #
 t = np.linspace(-tlim(N), tlim(N-1), N) #
-f = np.linspace(-flim(M), flim(M-1), M) #
+f = np.linspace(-flim(R), flim(R-1), R) #
+c = np.linspace(-flim(R), flim(R-1),R*3)
 #########################################
 
 #__Cajon__###############################
@@ -45,25 +47,31 @@ Unos = [1]*N                            #
 #__fft__#################################
 def F(fun_t):                           #
     fftR = abs(np.fft.fft(fun_t,n=M))/M #
-    fft0 = fftR[int((N+1)/2):]          #
-    fft1 = fftR[:int((N+1)/2)]          #
-    return np.concatenate((fft0,fft1))  #
+    fft0 = fftR#[int((M+1)/2):]         #
+    fft1 = fftR#[:int((M+1)/2)]         #
+    return np.concatenate((fft1,fft0))  #
+#########################################
+
+#__Tren_de_deltas__######################
+d = lambda z: np.piecewise(z,z==0,[1,0])#
+Del = d(c)                              #
 #########################################
 
 #__Ploteo__##############################
 #Funcion a plotear
-Fun = Unos 
+Fun = Seno
 fig, ax = plt.subplots(2)               #
 ax[0].plot(t, Fun, '.-')                #
-ax[1].plot(f, F(Fun), '.-')             #
+ax[1].plot(f, F(Fun), 'ro')             #
+ax[1].plot(c, Del, '-')
 ax[0].set_xlabel('Tiempo')              #
 ax[0].set_ylabel('Amplitud')            #
 ax[1].set_xlabel('Frecuencia')          #
 ax[1].set_ylabel('Amplitud')            #
-axLabels = np.linspace(-0.5, 0.5, N)    #
+axLabels = np.linspace(-1, 1, 2*M+1)    #
 ax[1].xaxis.set_ticks(axLabels)         #
 ax[1].grid()                            #
-plt.xlim(-0.5,0.5)                      #
+plt.xlim(-1,1)                          #
 plt.tight_layout()                      #
 plt.show()                              #
 #########################################
